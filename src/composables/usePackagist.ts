e46a215923338f4727e21e0a0aeb16976cbb70ee
Nay Thu Khant <developer@dev-users-MacBook-Pro.local>
2024-10-24 23:38:32 +0700

    fix: state

--- src/composables/usePackagist.ts
+++ src/composables/usePackagist.ts
@@ -1,54 +1,56 @@
-import { TProject, TProjects } from "../types/project";
-import { TPackagist } from "../types/packagist"; // Adjust the path as needed
-import { PACKAGIST_VENDORS } from "../constants";
+import {TProject, TProjects} from "../types/project";
+import {TPackagist} from "../types/packagist"; // Adjust the path as needed
+import {PACKAGIST_VENDORS} from "../constants";
 
 export const usePackagist = () => {
-  const packages: TProjects = [];
+    const packages: TProjects = [];
 
-  const fetchPackagesFromPackagist = async (): Promise<TProjects> => {
-    try {
-      await Promise.all(
-        PACKAGIST_VENDORS.map(async (organization) => {
-          const packagesUnderOrganizationResponse = await fetch(
-            `https://packagist.org/packages/list.json?vendor=${organization}&fields[]=type&fields[]=repository&fields[]=abandoned`
-          );
+    const fetchPackagesFromPackagist = async (): Promise<TProjects> => {
+        packages.length = 0;
 
-          const { packages: packagesUnderOrganization } =
-            (await packagesUnderOrganizationResponse.json()) as TPackagist.PackagesResponse;
+        try {
+            await Promise.all(
+                PACKAGIST_VENDORS.map(async (vendor) => {
+                    const packagesUnderOrganizationResponse = await fetch(
+                        `https://packagist.org/packages/list.json?vendor=${vendor}&fields[]=type&fields[]=repository&fields[]=abandoned`
+                    );
 
-          const packageInformationPromises = Object.keys(
-            packagesUnderOrganization
-          ).map(async (pkg) => {
-            const packageInformationResponse = await fetch(
-              `https://packagist.org/packages/${pkg}.json`
+                    const {packages: packagesUnderOrganization} =
+                        (await packagesUnderOrganizationResponse.json()) as TPackagist.PackagesResponse;
+
+                    const packageInformationPromises = Object.keys(
+                        packagesUnderOrganization
+                    ).map(async (pkg) => {
+                        const packageInformationResponse = await fetch(
+                            `https://packagist.org/packages/${pkg}.json`
+                        );
+
+                        const {package: packageInformation} =
+                            (await packageInformationResponse.json()) as TPackagist.PackageResponse;
+
+                        const version = Object.keys(packageInformation.versions)[0];
+
+                        return {
+                            name: packageInformation.name,
+                            description: packageInformation.description,
+                            downloads: packageInformation.downloads.total,
+                            homepage:
+                                packageInformation.versions[version]?.homepage ||
+                                packageInformation.repository,
+                        } as TProject;
+                    });
+
+                    packages.push(...(await Promise.all(packageInformationPromises)));
+                })
             );
+        } catch (error) {
+            console.error("Error fetching packages from packagist:", error);
+        }
 
-            const { package: packageInformation } =
-              (await packageInformationResponse.json()) as TPackagist.PackageResponse;
+        return packages;
+    };
 
-            const version = Object.keys(packageInformation.versions)[0];
-
-            return {
-              name: packageInformation.name,
-              description: packageInformation.description,
-              downloads: packageInformation.downloads.total,
-              homepage:
-                packageInformation.versions[version]?.homepage ||
-                packageInformation.repository,
-            } as TProject;
-          });
-
-          packages.push(...(await Promise.all(packageInformationPromises)));
-        })
-      );
-    } catch (error) {
-      console.error("Error fetching packages from packagist:", error);
-    }
-
-    return packages;
-  };
-
-  return {
-    fetchPackagesFromPackagist,
-  };
+    return {
+        fetchPackagesFromPackagist,
+    };
 };
