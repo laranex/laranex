31d8d574498ac80192f77e55dfcbfdd01e60a7c3
Nay Thu Khant <developer@dev-users-MacBook-Pro.local>
2025-01-06 15:48:34 +0700

    fix: github edit button

--- app.config.ts
+++ app.config.ts
@@ -5,19 +5,13 @@ export default defineAppConfig({
     description: 'Built by Developers, for Developers',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'laranex',
       github: 'laranex',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
     },
     github: {
-      dir: '.starters/default/content',
-      branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
+      dir: 'content',
+      branch: 'master',
+      repo: 'laranex',
+      owner: 'laranex',
       edit: true
     },
     aside: {
